e836f8a4e1669682fcdbe8a19803e81b6d6ed533
Brad Jorsch <brad.jorsch@automattic.com>
2020-12-02 12:45:22 -0500

    Add PHP 8 / PHPUnit 9.0 support
    PHPUnit doesn't support PHP 8 until version 9.0, so we need to be
    compatible with that while still maintaining compatibility with PHPUnit
    5.7.
    
    Fortunately the only thing WorDBLess\BaseTestCase does that's not
    compatible with both 5.7 and 9.0 is its use of `setUp` and `tearDown`,
    and we can work around that by using `@before` and `@after` instead of
    overriding those methods.
    
    Also this adds some tests to verify that the `@before` and `@after`
    methods are still called even if a subclass overrides `setUp`/`tearDown`
    or supplies its own `@before`/`@after`.

---
+++ tests/includes/setup-test-for-phpunit8+.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WorDBless;
+
+class Test_Setup_Teardown extends Test_Setup_Teardown_Base {
+
+	public function setUp(): void {
+		$this->setup_called = true;
+	}
+
+	public function tearDown(): void {
+		self::$teardown_called = true;
+	}
+
+	/**
+	 * @before
+	 */
+	public function custom_setup() {
+		$this->custom_setup_called = true;
+	}
+
+	/**
+	 * @after
+	 */
+	public function custom_teardown() {
+		self::$custom_teardown_called = true;
+	}
+
+}
