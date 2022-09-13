dbfc082eed5a5f86ce1a8b5dd44b878ae8b2233a
Brandon Kraft <public@brandonkraft.com>
2022-09-13 16:10:24 -0500

    PHPCS

--- src/install.php
+++ src/install.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * This is the installation script to copy the db dropin plugin into WordPress.
+ *
+ * @package Automattic/wordbless
+ */
 
-if ( ! is_dir('wordpress/wp-content' ) ) {
+if ( ! is_dir( 'wordpress/wp-content' ) ) {
 	mkdir( 'wordpress/wp-content' );
 }
 
-copy('src/dbless-wpdb.php', 'wordpress/wp-content/db.php');
+copy( 'src/dbless-wpdb.php', 'wordpress/wp-content/db.php' );
