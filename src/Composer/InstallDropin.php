208f2a108da16732998e6b1e22ad1e3779712b94
Brandon Kraft <public@brandonkraft.com>
2022-09-13 17:28:44 -0500

    Resolve wp-content folder failure on install (#54)
    * Ensure dir is present
    
    * Bump to WP 6+
    
    * Normalize composer.json
    
    * Update deps
    
    * Require WP 6.0.2+
    
    * Composer lock update
    
    * PHPCS
    
    * update phpunit dependency
    
    * Create InstallDropin.php
    
    * Delete install.php
    
    * Update Composer scripts
    
    * Fiy typo
    
    * Use tabs to indent
    
    * Fix CI
    
    Co-authored-by: Leo Germani <leogermani@automattic.com>
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

---
+++ src/Composer/InstallDropin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This is the installation script to copy the db dropin plugin into WordPress.
+ *
+ * @package Automattic/wordbless
+ */
+
+namespace WorDBless\Composer;
+
+class InstallDropin {
+	public static function copy() {
+		if ( ! is_dir( 'wordpress/wp-content' ) ) {
+			mkdir( 'wordpress/wp-content' );
+		}
+
+		copy( 'src/dbless-wpdb.php', 'wordpress/wp-content/db.php' );
+	}
+}
