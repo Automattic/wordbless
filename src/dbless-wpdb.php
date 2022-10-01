999d20a0b592d4dd960488de36bcaa2e9900d396
Cezar Popa <cezarpopa@users.noreply.github.com>
2022-10-01 11:16:14 +0100

    Cast parameter to string

--- src/dbless-wpdb.php
+++ src/dbless-wpdb.php
@@ -23,7 +23,7 @@ class Db_Less_Wpdb extends wpdb {
 	}
 
 	function _real_escape( $string ) {
-		return $this->add_placeholder_escape( is_string ( $string ) ? $string : '' );
+		return $this->add_placeholder_escape( (string) $string );
 	}
 
 	public function print_error( $str = '' ) {
