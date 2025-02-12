ff152d40fc5dfb2e81b1798643e078615b575f14
Brandon Kraft <public@brandonkraft.com>
2025-02-12 15:39:07 -0600

    Keep static analysis happy

--- src/BaseTestCase.php
+++ src/BaseTestCase.php
@@ -39,7 +39,7 @@ abstract class BaseTestCase extends TestCase {
 	 * @return void
 	 */
 	public function clear_uploads() {
-		$uploads_folder = ABSPATH . UPLOADS;
+		$uploads_folder = wp_get_upload_dir()['basedir'];
 		$scan           = glob( rtrim( $uploads_folder, '/' ) . '/*' );
 
 		foreach ( $scan as $path ) {
