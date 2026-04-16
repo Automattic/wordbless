5ed56df6be1b7b0794adcec6004c64c1a1fd293a
Igor Zinovyev <zinigor@gmail.com>
2026-04-16 18:55:38 +0300

    Fixed PHPCS errors.

--- src/Composer/InstallDropin.php
+++ src/Composer/InstallDropin.php
@@ -11,21 +11,21 @@ use Composer\Script\Event;
 
 class InstallDropin {
 	public static function copy( Event $event ) {
-		$extra = $event->getComposer()->getPackage()->getExtra();
-		$wpDir = $extra['wordpress-install-dir'] ?? 'wordpress';
+		$extra  = $event->getComposer()->getPackage()->getExtra();
+		$wp_dir = $extra['wordpress-install-dir'] ?? 'wordpress'; // phpcs:ignore WordPress.WP.CapitalPDangit.MisspelledInText -- keeping filesystem paths lowercase.
 
-		if ( ! is_dir( $wpDir . '/wp-content' ) ) {
-			mkdir( $wpDir . '/wp-content', 0777, true );
+		if ( ! is_dir( $wp_dir . '/wp-content' ) ) {
+			mkdir( $wp_dir . '/wp-content', 0777, true );
 		}
-		if ( ! is_dir( $wpDir . '/wp-content/themes' ) ) {
-			mkdir( $wpDir . '/wp-content/themes', 0777, true );
+		if ( ! is_dir( $wp_dir . '/wp-content/themes' ) ) {
+			mkdir( $wp_dir . '/wp-content/themes', 0777, true );
 		}
 
 		// Copy the dbless-wpdb.php file
-		copy( dirname( __DIR__ ) . '/dbless-wpdb.php', $wpDir . '/wp-content/db.php' );
+		copy( dirname( __DIR__ ) . '/dbless-wpdb.php', $wp_dir . '/wp-content/db.php' );
 
 		// Copy the SQLite database integration plugin
-		$sqlite_plugin_dir = $wpDir . '/wp-content/plugins/wp-sqlite-integration';
+		$sqlite_plugin_dir = $wp_dir . '/wp-content/plugins/wp-sqlite-integration';
 		if ( ! is_dir( $sqlite_plugin_dir ) ) {
 			mkdir( $sqlite_plugin_dir, 0777, true );
 		}
